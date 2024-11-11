0c05273965419f637cb09b72eb40d0fe4a04be9e
diegomoers <diegomoers@gmail.com>
2024-11-11 12:59:54 +0100

    Create Week1.R

---
+++ WEEK1/Week1.R
@@ -0,0 +1,5 @@
+setwd("~/Desktop/EUR/BLOK2/Data and HR Analytics")
+ceo_diary <- read.csv("~/Desktop/EUR/BLOK2/Data and HR Analytics/Data/survey_response_data.csv")
+#worked with setting wd
+View(ceo_diary)
+CEO_Diary[1:15,c(1:5,37, 39, 40)]
